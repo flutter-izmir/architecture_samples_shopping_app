0ad1f773315453b4acc630b574d4b9273ab41ab5
Mirkan <mirkancaliskan@gmail.com>
2020-05-01 18:25:43 +0300

    Provide blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
+import 'package:architecture_samples_shopping_app/blocs/cart/cart_bloc.dart';
+import 'package:architecture_samples_shopping_app/blocs/catalog/bloc.dart';
 import 'package:architecture_samples_shopping_app/pages/catalog_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CatalogBloc()..add(LoadCatalog()),
+        ),
+        BlocProvider(
+          create: (context) => CartBloc()..add(LoadCart()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: CatalogPage(),
       ),
-      home: CatalogPage(),
     );
   }
 }
