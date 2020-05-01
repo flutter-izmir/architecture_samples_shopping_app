c6b16338550ee59ed985c23e7aaee2d673c3d365
Mirkan <mirkancaliskan@gmail.com>
2020-05-01 18:25:32 +0300

    Refactor catalog page

--- lib/pages/catalog_page.dart
+++ lib/pages/catalog_page.dart
@@ -1,6 +1,9 @@
+import 'package:architecture_samples_shopping_app/blocs/cart/cart_bloc.dart';
+import 'package:architecture_samples_shopping_app/blocs/catalog/catalog_bloc.dart';
 import 'package:architecture_samples_shopping_app/models/item_model.dart';
 import 'package:architecture_samples_shopping_app/pages/cart_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CatalogPage extends StatefulWidget {
   @override
@@ -8,10 +11,6 @@ class CatalogPage extends StatefulWidget {
 }
 
 class _CatalogPageState extends State<CatalogPage> {
-  final List<Item> catalog = Item.catalog;
-
-  List<Item> saved = List<Item>();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,76 +18,115 @@ class _CatalogPageState extends State<CatalogPage> {
         title: Text("Catalog"),
         elevation: 0,
         actions: <Widget>[
-          Stack(
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () {
-                  Navigator.push<List<Item>>(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CartPage(
-                        items: saved,
+          BlocBuilder<CartBloc, CartState>(
+            builder: (context, state) {
+              if (state is CartLoading) {
+                return CircularProgressIndicator();
+              }
+              if (state is CartLoaded) {
+                return Stack(
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.shopping_cart),
+                      onPressed: () {
+                        Navigator.push<List<Item>>(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CartPage(),
+                          ),
+                        );
+                      },
+                    ),
+                    Positioned(
+                      left: 5,
+                      top: 5,
+                      child: Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                          color: Colors.red[700],
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(1.0),
+                            child: Text("${state.cart.length}"),
+                          ),
+                        ),
                       ),
                     ),
-                  ).then((List<Item> items) {
-                    setState(() {
-                      saved = items;
-                    });
-                  });
-                },
-              ),
-              Positioned(
-                left: 5,
-                top: 5,
-                child: Container(
-                  width: 20,
-                  height: 20,
-                  decoration: BoxDecoration(
-                    color: Colors.red[700],
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(1.0),
-                      child: Text("${saved.length}"),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+                  ],
+                );
+              }
+              return Text("An error occurred");
+            },
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: catalog.length,
-        itemBuilder: (context, index) {
-          final item = catalog[index];
-          final alreadySaved = saved.contains(item);
-          return ListTile(
-            title: Text(
-              item.name,
-              style: TextStyle(color: Colors.blue[700]),
-            ),
-            subtitle: Text("${item.price} \$"),
-            trailing: IconButton(
-              color: alreadySaved ? Colors.blue[700] : null,
-              icon: Icon(alreadySaved
-                  ? Icons.remove_shopping_cart
-                  : Icons.add_shopping_cart),
-              onPressed: () {
-                setState(() {
-                  if (alreadySaved) {
-                    saved.remove(item);
-                  } else {
-                    saved.add(item);
-                  }
-                });
+      body: BlocBuilder<CatalogBloc, CatalogState>(
+        builder: (context, state) {
+          if (state is CatalogLoading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is CatalogReady) {
+            final catalog = state.items;
+            return ListView.builder(
+              itemCount: catalog.length,
+              itemBuilder: (context, index) {
+                final item = catalog[index];
+                return ListTile(
+                  title: Text(
+                    item.name,
+                    style: TextStyle(color: Colors.blue[700]),
+                  ),
+                  subtitle: Text("${item.price} \$"),
+                  trailing: _AddButton(item: item),
+                );
               },
-            ),
-          );
+            );
+          }
+          if (state is CatalogError) {
+            return Center(
+              child: Text("An error occurred"),
+            );
+          }
+          return Container();
         },
       ),
+    );
+  }
+}
+
+class _AddButton extends StatelessWidget {
+  _AddButton({@required this.item});
+  final Item item;
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CartBloc, CartState>(
+      builder: (context, state) {
+        if (state is CartLoading) {
+          return CircularProgressIndicator();
+        }
+        if (state is CartLoaded) {
+          final alreadyAdded = state.cart.contains(item);
+          return IconButton(
+            color: alreadyAdded ? Colors.blue[700] : null,
+            icon: Icon(
+              alreadyAdded
+                  ? Icons.remove_shopping_cart
+                  : Icons.add_shopping_cart,
+            ),
+            onPressed: alreadyAdded
+                ? () => BlocProvider.of<CartBloc>(context)
+                    .add(RemoveCart(item: item))
+                : () =>
+                    BlocProvider.of<CartBloc>(context).add(AddCart(item: item)),
+          );
+        }
+        return Text("Something went wrong");
+      },
     );
   }
 }
